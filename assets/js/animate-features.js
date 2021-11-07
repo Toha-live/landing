b860903f08b6bdf7efed7123748d91e72a084e8c
Gabriel Jauffret <gabjauf@hotmail.fr>
2021-11-07 22:59:43 +0100

    fix(features): remove animation on mobile app

--- assets/js/animate-features.js
+++ assets/js/animate-features.js
@@ -1,32 +1,34 @@
-gsap.timeline()
+ScrollTrigger.matchMedia({
+  "(min-width: 800px)": function () {
+    gsap.to("#features", {
+      scrollTrigger: {
+        trigger: "#features",
+        scrub: true,
+        pin: true,
+        start: "center center",
+        end: "bottom -100%",
+        ease: "power2"
+      }
+    });
+    
+    gsap.from(".feature-image", {
+      opacity: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: ".feature-image",
+        start: "top +50%",
+        scrub: 1,
+      }
+    });
 
-gsap.to("#features", {
-  scrollTrigger: {
-    trigger: "#features",
-    scrub: true,
-    pin: true,
-    start: "center center",
-    end: "bottom -100%",
-    ease: "power2"
+    gsap.utils.toArray(".feature").forEach(elem => {
+      gsap.from(elem, { opacity: 0, duration: 1, scrollTrigger: {
+        trigger: elem,
+        scrub: 1,
+        stagger: 0.25,
+        start: "top top",
+        end: "bottom center",
+      } });
+    })
   }
-});
-
-gsap.from(".feature-image", {
-  opacity: 0,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".feature-image",
-    start: "top +50%",
-    scrub: 1,
-  }
-});
-
-gsap.utils.toArray(".feature").forEach(elem => {
-  gsap.from(elem, { opacity: 0, duration: 1, scrollTrigger: {
-    trigger: elem,
-    scrub: 1,
-    stagger: 0.25,
-    start: "top top",
-    end: "bottom center",
-  } });
 })
